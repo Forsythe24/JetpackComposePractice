a3686cadd19cc9dfc8e2bc75430e723f19c8da58
Forsythe24 <miron.solopov@yandex.ru>
2024-06-25 20:28:19 +0300

    left-overs

--- app/src/main/java/com/plcoding/meditationuiyoutube/ui/HomeScreen.kt
+++ app/src/main/java/com/plcoding/meditationuiyoutube/ui/HomeScreen.kt
@@ -5,19 +5,19 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -26,14 +26,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.plcoding.meditationuiyoutube.BottomMenuContent
 import com.plcoding.meditationuiyoutube.Feature
-import com.plcoding.meditationuiyoutube.R
 import com.plcoding.meditationuiyoutube.standardQuadFromTo
 import com.plcoding.meditationuiyoutube.ui.theme.*
+import com.solopov.jetpack_compose_practice.R
 
+@Preview
 @ExperimentalFoundationApi
 @Composable
 fun HomeScreen() {
@@ -176,11 +178,11 @@ fun GreetingSection(
         ) {
             Text(
                 text = "Good morning, $name",
-                style = MaterialTheme.typography.h2
+                style = MaterialTheme.typography.headlineMedium
             )
             Text(
                 text = "We wish you have a good day!",
-                style = MaterialTheme.typography.body1
+                style = MaterialTheme.typography.bodyLarge
             )
         }
         Icon(
@@ -238,11 +240,11 @@ fun CurrentMeditation(
         Column {
             Text(
                 text = "Daily Thought",
-                style = MaterialTheme.typography.h2
+                style = MaterialTheme.typography.headlineMedium
             )
             Text(
                 text = "Meditation • 3-10 min",
-                style = MaterialTheme.typography.body1,
+                style = MaterialTheme.typography.bodyLarge,
                 color = TextWhite
             )
         }
@@ -264,17 +266,16 @@ fun CurrentMeditation(
     }
 }
 
-@ExperimentalFoundationApi
 @Composable
 fun FeatureSection(features: List<Feature>) {
     Column(modifier = Modifier.fillMaxWidth()) {
         Text(
             text = "Features",
-            style = MaterialTheme.typography.h1,
+            style = MaterialTheme.typography.headlineLarge,
             modifier = Modifier.padding(15.dp)
         )
         LazyVerticalGrid(
-            cells = GridCells.Fixed(2),
+            columns = GridCells.Fixed(2),
             contentPadding = PaddingValues(start = 7.5.dp, end = 7.5.dp, bottom = 100.dp),
             modifier = Modifier.fillMaxHeight()
         ) {
@@ -354,7 +355,7 @@ fun FeatureItem(
         ) {
             Text(
                 text = feature.title,
-                style = MaterialTheme.typography.h2,
+                style = MaterialTheme.typography.headlineMedium,
                 lineHeight = 26.sp,
                 modifier = Modifier.align(Alignment.TopStart)
             )
